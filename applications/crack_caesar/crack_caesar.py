25d519ba1ad3802b1b089424fd83a2aee136735e
Monica Bustamante <lilianalambda@gmail.com>
2020-08-05 15:27:06 -0700

    Hast Table python

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,32 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
+#https://thispointer.com/python-map-function-explained-with-examples/
+#https://openbookproject.net/thinkcs/python/english3e/lists.html
 
-# Your code here
+alphabet_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+             'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
 
+
+def letter_frequency(words, letters= alphabet_list):
+    cache = {}
+    for letter in words:
+        if letter not in letters:
+            continue
+        if letter not in cache:
+            cache[letter] = 1
+        else:
+            cache[letter] += 1
+    return sorted(cache.items(), key=lambda x: x[1], reverse=True) #sort by value
+
+def crack_ceasar(words):
+    letter_period = letter_frequency(words)
+    match = {letter_period[i][0]:alphabet_list[i]  #Dictionary
+                for i in range (len(letter_period))}
+    emission = ''.join(map(lambda x: match[x] if x in match else x, words)) ## Reverse each string in the list using lambda function & map()
+    print(emission)
+
+if __name__ == "__main__":
+    # access data cyphertext 
+    with open('applications/crack_caesar/ciphertext.txt') as file:
+        ciphertext = file.read()   
+    print(crack_ceasar(ciphertext))
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,30 @@
 # Your code here
+#https://careerkarma.com/blog/python-sort-a-dictionary-by-value/#:~:text=To%20sort%20a%20dictionary%20by%20value%20in%20Python%20you%20can,Dictionaries%20are%20unordered%20data%20structures.
+#https://pythonexamples.org/python-count-number-of-words-in-text-file/
+#https://stackoverflow.com/questions/21107505/word-count-from-a-txt-file-program
+
+with open('applications/histo/robin.txt') as file:
+    robin = file.read() 
+print(robin)
+words = robin.split()
+print('number of words in text file:', len(words))
+
+wordCounter = {}
+
+with open('applications/histo/robin.txt','r') as fh:
+  for line in fh:
+    word_list = line.replace(',','').replace('\'','').replace('.','').lower().split() #split line into list
+    for word in word_list:
+      if word not in wordCounter: #add word into dictionary
+        wordCounter[word] = 1
+      else:
+        wordCounter[word] = wordCounter[word] + 1 #update count
+
+print('{:15}{:3}'.format('Word','Count'))
+print('-' * 18)
+for  (word,occurance)  in wordCounter.items(): #occurrence
+  print('{:15}{:3}'.format(word,occurance))
+
+
+
 
